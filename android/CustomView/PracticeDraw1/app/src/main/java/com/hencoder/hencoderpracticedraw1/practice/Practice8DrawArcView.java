caaa04c6ecdbe3a6847b79af947d82145b0aa65d
JJC <928692385@qq.com>
2017-07-14 19:20:54 +0800

    empty msg

--- android/CustomView/PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ android/CustomView/PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice8DrawArcView extends View {
+    private int mWidth;
+    private Paint mPaint = new Paint();
+    private int mHeight;
 
     public Practice8DrawArcView(Context context) {
         super(context);
@@ -25,5 +30,28 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        //弧形
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(2);
+        mPaint.setStyle(Paint.Style.STROKE);
+        RectF mArcLine = new RectF(mHeight * 0.4f, mHeight * 0.2f, mWidth * 0.7f, mHeight * 0.6f);
+        //        startAngle   开始角度 已圆心为 0度   sweepAngle  增加多少度
+        canvas.drawArc(mArcLine, -180, 60, false, mPaint);
+
+        //圆心弧形
+        mPaint.setStyle(Paint.Style.FILL);
+        //从-110 度开始，绘制90个度数 也就是 -110° -- -20°  240°- 330°
+        canvas.drawArc(mArcLine, -110, 90, true, mPaint);
+
+        //非圆心弧形
+        //从25度绘制130度 也就是  25° --- 155°
+        canvas.drawArc(mArcLine, 25, 130, false, mPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 }
--- android/CustomView/PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ android/CustomView/PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,11 +2,19 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+    private int mWidth;
+    private Paint mPaint = new Paint();
+    private Path mPath = new Path();
+    private int mHeight;
 
     public Practice9DrawPathView(Context context) {
         super(context);
@@ -23,7 +31,26 @@ public class Practice9DrawPathView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        mPaint.setAntiAlias(true);
+        RectF mArcLine = new RectF(200, 0, 400, 200);
+        mPaint.setStrokeWidth(2f);
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        //1.移动到中间
+        mPath.moveTo(mWidth * 0.4f, 100);
+        //2.先绘制左边的圆
+        mPath.addArc(mArcLine, -225, 225);
+
+        //绘制右边的圆
+        mPaint.setColor(Color.YELLOW);
+        mPath.arcTo(400, 0, 200, 400, -225, 225, false);
+        canvas.drawPath(mPath, mPaint);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 }
