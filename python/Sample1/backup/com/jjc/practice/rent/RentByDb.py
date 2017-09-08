6b4a8882a959a39154e51ee2021745143b744fec
JJC <928692385@qq.com>
2017-09-08 18:27:22 +0800

    修改一部分

--- python/Sample1/backup/com/jjc/practice/rent/RentByDb.py
+++ python/Sample1/backup/com/jjc/practice/rent/RentByDb.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import pymysql
@@ -13,34 +12,48 @@ def queryData():
     db = pymysql.connect("localhost", "root", "root", "python_58_rent", charset="utf8")
     # 创建游标
     cursor = db.cursor()
-    # sql
+    # sql  可以优化
     sql = "select * from rent_58_list"
+    # 链接
+    urls = []
     try:
-        print("begin query data")
         cursor.execute(sql)
         result = cursor.fetchall()
         for row in result:
-            id = row[0]
-            name = row[1]
-            master = row[2]
-            add = row[3]
-            link = row[4]
-            money = row[5]
-            room = row[6]
-            print("===============start===================")
-            print("编号：", id)
-            print("名称：", name)
-            print("房东：", master)
-            print("地址：", add)
-            print("链接：", link)
-            print("价格：", money)
-            print("房室：", room)
-            print("================end====================")
-
+            # id = row[0]
+            # name = row[1]
+            # master = row[2]
+            # add = row[3]
+            # link = row[4]
+            # money = row[5]
+            # room = row[6]
+            # print("===============start===================")
+            # print("编号：", id)
+            # print("名称：", name)
+            # print("房东：", master)
+            # print("地址：", add)
+            # print("链接：", link)
+            # print("价格：", money)
+            # print("房室：", room)
+            # print("================end====================")
+            urls.append(row[4])
     except Exception as e:
         db.rollback()
         print(e)
     db.close()
+    return urls
+
+
+def spiderDetails(urls):
+    """
+    查询详情页面
+    :param urls: 目标url
+    :return:
+    """
+
 
 if __name__ == "__main__":
-    queryData()
+    # 查询到所有的链接
+    urls = queryData()
+    # 查询详情
+    spiderDetails(urls)
