b335e7b3304a948d9a97a027b20c12726a4bc684
JJC <928692385@qq.com>
2017-09-27 10:55:44 +0800

    58

--- python/scrapyPractice1/scrapyPractice1/pipelines.py
+++ python/scrapyPractice1/scrapyPractice1/pipelines.py
@@ -5,8 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymysql
+
 
 class Scrapypractice1Pipeline(object):
+    def __init__(self):
+        print("初始化")
+
     def process_item(self, item, spider):
         print(item)
         return item
--- python/scrapyPractice1/scrapyPractice1/spiders/rent58.py
+++ python/scrapyPractice1/scrapyPractice1/spiders/rent58.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from numpy import unicode
-
+from  scrapy import Request
 from scrapyPractice1.items import RentDetails
 
 
 class Rent58Spider(scrapy.Spider):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+    }
+
     name = 'rent58'
     allowed_domains = ['http://sz.58.com/']
     start_urls = ['http://sz.58.com/xinanlu/chuzu/0/b9/']
@@ -14,7 +18,6 @@ class Rent58Spider(scrapy.Spider):
         # 获取所有的li
         lis = response.xpath("/html/body/div[3]/div[1]/div[5]/div[2]/ul/li")
         # 所有的数据
-        datas = []
 
         for li in lis:
             try:
@@ -22,7 +25,7 @@ class Rent58Spider(scrapy.Spider):
                 # 房源名称
                 name = li.xpath("./div[2]/h2/a/text()").extract()[0]
                 item["name"] = name.strip()
-                item["name"] = unicode(name, "utf-8")
+                # item["name"] = unicode(name, "utf-8")
                 # 详情链接
                 link = li.xpath("./div[2]/h2/a/@href").extract()[0]
                 item["link"] = link.strip()
@@ -39,7 +42,15 @@ class Rent58Spider(scrapy.Spider):
                 master = li.xpath("./div[2]/p[3]/text()").extract()[0]
                 item["master"] = master.strip()
                 # 加入数据
-                datas.append(item)
+                yield item
             except Exception as e:
                 print(e)
-        return datas
+            try:
+                # 获取下一页的数据
+                nextUrl = response.xpath(
+                    "//*[@id='bottom_ad_li']/div[2]/a[@class='next']/@href").extract()
+                if nextUrl:
+                    print("开始下", nextUrl[0])
+                    yield Request(url=nextUrl[0], headers=self.headers)
+            except Exception as e:
+                print("获取下一页失败", e)
