ecd9f9e6ae4d0a88fb6a648d5ea0e65dcabd63e9
JJC <928692385@qq.com>
2017-09-27 15:14:22 +0800

    暂时完成

---
+++ python/MeiziSpider/MeiziSpider/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MeizispiderItem(scrapy.Item):
+    name = scrapy.Field()
+    image_urls = scrapy.Field()
+    url = scrapy.Field()
---
+++ python/MeiziSpider/MeiziSpider/spiders/Spider.py
@@ -0,0 +1,51 @@
+from scrapy.spider import CrawlSpider, Rule, Request
+from scrapy.linkextractors import LinkExtractor
+
+from MeiziSpider.items import MeizispiderItem
+
+
+class MeiziSpider(CrawlSpider):
+    name = 'MeiziSpider'
+    allowed_domains = ['mzitu.com']
+    start_urls = ['http://www.mzitu.com/']
+    img_urls = []
+    rules = (
+        Rule(LinkExtractor(allow=('http://www.mzitu.com/\d{1,6}',),
+                           deny='http://www.mzitu.com/\d{1,6}/\d{1,6}'), callback='parse_item',
+             follow=True),
+    )
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                      'like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+    }
+
+    def parse_item(self, response):
+        """
+        :param response: 下载器返回的response
+        :return:
+        """
+        item = MeizispiderItem()
+        # max_num为页面最后一张图片的位置
+        max_num = response.xpath(
+            "descendant::div[@class='main']/div[@class='content']/div[@class='pagenavi']/a[last("
+            ")-1]/span/text()").extract_first(
+            default="N/A")
+        item['name'] = response.xpath("./*//div[@class='main']/div[1]/h2/text()").extract_first(
+            default="N/A")
+        item['url'] = response.url
+        for num in range(1, int(max_num)):
+            # page_url 为每张图片所在的页面地址
+            page_url = response.url + '/' + str(num)
+            yield Request(page_url, callback=self.img_url)
+        item['image_urls'] = self.img_urls
+        yield item
+
+    def img_url(self, response):
+        """取出图片URL 并添加进self.img_urls列表中
+        :param response:
+        :param img_url 为每张图片的真实地址
+        """
+        img_urls = response.xpath(
+            "descendant::div[@class='main-image']/descendant::img/@src").extract()
+        for img_url in img_urls:
+            self.img_urls.append(img_url)
