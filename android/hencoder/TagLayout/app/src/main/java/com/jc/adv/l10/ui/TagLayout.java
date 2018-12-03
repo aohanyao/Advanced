3a45601c8b59127d82e5e5266d45291c5e414025
江俊超 <928692385@qq.com>
2018-12-03 22:14:21 +0800

    初步完成 TagLayout
    TagLayout

--- android/hencoder/TagLayout/app/src/main/java/com/jc/adv/l10/ui/TagLayout.java
+++ android/hencoder/TagLayout/app/src/main/java/com/jc/adv/l10/ui/TagLayout.java
@@ -1,9 +1,13 @@
 package com.jc.adv.l10.ui;
 
 import android.content.Context;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 江俊超 on 2018/12/3.
@@ -11,10 +15,14 @@ import android.view.ViewGroup;
  * Description:
  * ChangeLog:
  * ----------------------------------------------------
- * 1. 测量
+ * 自定义View中，测量，布局是一种思路。
  * ----------------------------------------------------
  */
+
 public class TagLayout extends ViewGroup {
+    // 用来存每个View的位置
+    private List<RectF> mTagBounds = new ArrayList<>();
+
     public TagLayout(Context context) {
         super(context);
     }
@@ -36,15 +44,51 @@ public class TagLayout extends ViewGroup {
         // 行高度
         float lineHeight = 0;
         // 已经使用的宽度
-        int widthUse= getWidth();
+        int widhtUse = 0;
+        // 整个View的宽度
+        int widhtSpec = MeasureSpec.getSize(widthMeasureSpec);
+
+        //已经使用了的高度
+        int heightUse = 0;
+
+
         // 遍历所有的子View
         for (int i = 0; i < getChildCount(); i++) {
             // 获取子View
             View childView = getChildAt(i);
 
             // 测量子view
-//            measureChildWithMargins(childView,widthMeasureSpec,widthUse//);
+            measureChildWithMargins(childView, widthMeasureSpec, 0, heightMeasureSpec, heightUse);
 
+            // 进行换行处理
+            if ((widhtUse + childView.getMeasuredWidth() > widhtSpec)) {
+                // 增加高度
+                heightUse = (int) (heightUse + lineHeight);
+                widhtUse = 0;
+                // 重新处理
+                measureChildWithMargins(childView, widthMeasureSpec, 0, heightMeasureSpec, heightUse);
+
+            }
+
+            // 取出存储坐标的矩形
+            RectF bound;
+            if (mTagBounds.size() <= i) {
+                bound = new RectF();
+                mTagBounds.add(bound);
+            } else {
+                bound = mTagBounds.get(i);
+            }
+
+            // 设置位置
+            bound.left = widhtUse;
+            bound.top = heightUse;
+            bound.right = widhtUse + childView.getMeasuredWidth();
+            bound.bottom = heightUse + childView.getMeasuredHeight();
+
+            // 对比行高
+            lineHeight = Math.max(childView.getMeasuredHeight(), lineHeight);
+            // 宽度的使用要加上本次 子view的宽度
+            widhtUse = widhtUse + childView.getMeasuredWidth();
         }
 
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -52,6 +96,16 @@ public class TagLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+        for (int i = 0; i < getChildCount(); i++) {
+            RectF bound = mTagBounds.get(i);
+            View childView = getChildAt(i);
+            childView.layout((int) bound.left, (int) bound.top, (int) bound.right, (int) bound.bottom);
+        }
     }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
 }
